2edaeec128a247729d5052ecb1151d5550aa5063
isturcanu <91874438+isturcanu@users.noreply.github.com>
2022-10-27 18:02:53 +0300

    configure eureka client

--- src/main/java/com/example/authservice/AuthServiceApplication.java
+++ src/main/java/com/example/authservice/AuthServiceApplication.java
@@ -2,9 +2,10 @@ package com.example.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/authservice/config/SecurityConfig.java
+++ src/main/java/com/example/authservice/config/SecurityConfig.java
@@ -1,12 +1,10 @@
 package com.example.authservice.config;
 
 
-import com.example.authservice.domain.Role;
 import com.example.authservice.security.jwt.JwtFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
